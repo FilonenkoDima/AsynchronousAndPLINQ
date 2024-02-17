1461f58442de3bf82f29e456e223b58e13871239
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-18 00:52:57 +0200

    The Lock Statement

---
+++ TheLockStatement_2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TheLockStatement_2
+{
+	internal class Program
+	{
+		// shared private fields
+		private static int value1 = 1;
+		private static int value2 = 1;
+
+		// synchronisation object
+		private static object syncObj = new object();
+
+		// thread work method
+		public static void DoWork()
+		{
+			bool lockTaken = false;
+			try
+			{
+				//Monitor.Enter(syncObj, ref lockTaken);
+
+				#region Monitor.TryEnter
+				Monitor.TryEnter(syncObj, TimeSpan.FromMilliseconds(50), ref lockTaken);
+				#endregion
+
+				if (value2 > 0)
+				{
+                    Console.WriteLine(value1 / value2);
+					value2 = 0;
+                }
+			}
+			finally
+			{
+				if (lockTaken)
+				{
+					Monitor.Exit(syncObj);
+				}
+			}
+		}
+		static void Main(string[] args)
+		{
+			// start two threads
+			Thread t1 = new Thread(DoWork);
+			Thread t2 = new Thread(DoWork);
+			t1.Start();
+			t2.Start();
+		}
+	}
+}
---
+++ TheLockStatement_3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TheLockStatement_3
+{
+	internal class Program
+	{
+		// shared private fields
+		private static int value1 = 1;
+		private static int value2 = 1;
+
+		// synchronisation object
+		private static object syncObj = new object();
+
+		// thread work method
+		public static void DoWork()
+		{
+			lock (syncObj)
+			{
+				if (value2 > 0)
+				{
+					DoTheDivision();
+					value2 = 0;
+				}
+			}
+		}
+
+		// helper method to do the division
+		public static void DoTheDivision()
+		{
+			lock (syncObj)
+			{
+                Console.WriteLine(value1 / value2);
+            }
+		}
+		static void Main(string[] args)
+		{
+			// start two threads
+			Thread t1 = new Thread(DoWork);
+			Thread t2 = new Thread(DoWork);
+			t1.Start();
+			t2.Start();
+		}
+	}
+}
