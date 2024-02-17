d3bef9a4be778f9093d80507553243287f8b199f
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-02-18 00:16:05 +0200

    Race Conditions

---
+++ RaceConditions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RaceConditions
+{
+	internal class Program
+	{
+		public static int i = 0;
+
+		public static void DoWork()
+		{
+			for ( i = 0; i < 5; i++)
+			{
+				Console.Write("*");
+			}
+		}
+		static void Main(string[] args)
+		{
+			// start thread to display 5 stars
+			Thread t = new Thread(DoWork);
+			t.Start();
+
+			// display 5 additional stars
+			DoWork();
+		}
+	}
+}
